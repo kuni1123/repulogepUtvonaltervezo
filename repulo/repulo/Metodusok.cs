9baf5bf614d51ed887ef7031ed7c4321fa9e2492
kuni1123 <33810970+kuni1123@users.noreply.github.com>
2018-04-18 11:17:39 +0200

    Első lépések
    Adatok beolvasása beillesztés az adatbázisba és átszállás nélküli útvonaltervezés

---
+++ repulo/repulo/Metodusok.cs
@@ -0,0 +1,65 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repulo
+{
+    class Metodusok
+    {
+        public struct ut
+        {
+           public string honnan;
+           public string hova;
+           public string km;
+           public string ido;
+           public string tarsasag;
+        }
+        public static string kapcsolat = "SERVER=localhost;DATABASE=repulo;UID=root;PASSWORD=mysql;";
+        public static MySqlCommand parancs = new MySqlCommand();
+        public static MySqlConnection cnn = new MySqlConnection(kapcsolat);
+        public MySqlDataAdapter da = new MySqlDataAdapter();
+        public DataSet ds = new DataSet();
+        public List<ut> atszallas = new List<ut>();
+
+        public void lekerdezes(string lekerdezes) {
+            try
+            {
+                DataTable dt = new DataTable();
+                cnn.Open();
+                parancs = new MySqlCommand(lekerdezes, cnn);
+                da.SelectCommand = parancs;
+                da.Fill(dt);
+                ds.Tables.Add(dt);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cnn.Close();
+            }
+        }
+        public void kereses(string honnan, string hova)
+        {
+            for (int i = 0; i < ds.Tables[1].Rows.Count; i++)
+            {
+                if(honnan == ds.Tables[1].Rows[i][1].ToString() && hova == ds.Tables[1].Rows[i][2].ToString())
+                {
+                    ut utvonal = new ut();
+                    utvonal.honnan = ds.Tables[1].Rows[i][1].ToString();
+                    utvonal.hova = ds.Tables[1].Rows[i][2].ToString();
+                    utvonal.km = ds.Tables[1].Rows[i][3].ToString();
+                    utvonal.ido = ds.Tables[1].Rows[i][4].ToString();
+                    utvonal.tarsasag = ds.Tables[1].Rows[i][5].ToString();
+                    atszallas.Add(utvonal);
+                    //Console.WriteLine(utvonal.honnan+";"+utvonal.hova+";"+utvonal.km+";"+utvonal.ido+";"+utvonal.tarsasag);
+                }
+            }
+        }
+    }
+}
---
+++ repulo/repulo/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace repulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Metodusok met = new Metodusok();
+
+            string lekerdezes = "SELECT * FROM varos;";
+            met.lekerdezes(lekerdezes);
+            lekerdezes = "SELECT * FROM jaratok;";
+            met.lekerdezes(lekerdezes);
+            for (int i = 0; i < met.ds.Tables[0].Rows.Count; i++)
+            {
+                Console.Write(met.ds.Tables[0].Rows[i][0]);
+                Console.WriteLine(met.ds.Tables[0].Rows[i][1]);
+            }
+            for (int i = 0; i < met.ds.Tables[1].Rows.Count; i++)
+            {
+                Console.Write(met.ds.Tables[1].Rows[i][0] + ";");
+                Console.Write(met.ds.Tables[1].Rows[i][1] + ";");
+                Console.Write(met.ds.Tables[1].Rows[i][2] + ";");
+                Console.Write(met.ds.Tables[1].Rows[i][3] + ";");
+                Console.Write(met.ds.Tables[1].Rows[i][4] + ";");
+                Console.WriteLine(met.ds.Tables[1].Rows[i][5]);
+            }
+            Console.WriteLine("Járatok és városok beolvasása Kész!");
+            Console.WriteLine("Kérlek add meg, hogy honnan szeretnél indulni:");
+            bool honnanVan = false;
+            string honnan = "";
+            string hova = "";
+            while (!honnanVan)
+            {
+                honnan = Console.ReadLine();
+                for (int i = 0; i < met.ds.Tables[1].Rows.Count; i++)
+                {
+                    if (met.ds.Tables[1].Rows[i][1].ToString() == honnan)
+                    {
+                        honnanVan = true;
+                    }
+                }
+                if (!honnanVan)
+                {
+                    Console.WriteLine("Innen nem indulnak járatok: "+honnan);
+                }
+            }
+
+            Console.WriteLine("És most add meg, hogy hova szeretnél utazni:");
+            bool hovaVan = false;
+            while (!hovaVan)
+            {
+                hova = Console.ReadLine();
+                for (int i = 0; i < met.ds.Tables[1].Rows.Count; i++)
+                {
+                    if (met.ds.Tables[1].Rows[i][2].ToString() == hova)
+                    {
+                        hovaVan = true;
+                    }
+                }
+                if (!hovaVan)
+                {
+                    Console.WriteLine("Nem indul "+honnan+"->" + hova+" járat!");
+                }
+            }
+            met.kereses(honnan, hova);
+            Console.WriteLine("A legrövidebb útvonal:");
+            for (int i = 0; i < met.atszallas.Count; i++)
+            {
+                Metodusok.ut utvonal = new Metodusok.ut();
+                utvonal = met.atszallas[i];
+                Console.WriteLine("Felszállás: "+utvonal.honnan+" Leszállás: "+utvonal.hova+" Távolság: "+utvonal.km+" km Időtartam "+utvonal.ido+" Társaság: "+utvonal.tarsasag);
+            }
+            Console.ReadKey();
+        }
+    }
+}
